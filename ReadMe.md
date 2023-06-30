9a9af762ea4654672439c71e088e6dcf2eb1636e
JULIANA PANSOWY FURNO <47350201@ort>
2023-06-30 12:01:57 -0300

    swagger

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,6 @@
-# Descripcion de nuestro proyecto
-### - Luego de saabrir car el proyecto de GitHub, abrimos la base de datos y la conectamos con el proyecto.
-### - Luego tenemos que instalar las dependencias, para hacer esto debemos hacer npm i
+# Documentacion de nuestro proyecto
+#### - Abrir el proyecto de GitHub, abrimos la base de datos y la conectamos con el proyecto.
+#### - Instalar las dependencias, para hacer esto debemos hacer npm i
+#### - Abrir la terminal para ejectutar el codigo
+### Swagger
+
--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import PersonajeRouter from "./src/controllers/personajesController.js";
 import { jwtStrategy } from "./src/common/jwt.strategy.js";
 import authController from "./src/controllers/authController.js";
 import 'dotenv/config'
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
 
 const app = express();
 const port = 5000;
@@ -18,6 +25,43 @@ app.use(express.json());
 app.use("/movie", PeliculaRouter);
 app.use("/character", PersonajeRouter);
 app.use("/auth/login", authController);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
